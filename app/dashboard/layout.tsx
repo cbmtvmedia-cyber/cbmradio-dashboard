bc0d3a0fabf6415e12bf7c1c1156ac8d43be5633
CBM <cbmtvmedia@gmail.com>
2026-07-29 10:02:47 +0300

    Set isCollapsed prop of Sidebar to false

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -211,6 +211,7 @@ export default function DashboardLayout({
             themeMode={themeMode}
             setThemeMode={setThemeMode}
             resolvedDark={resolvedDark}
+            isCollapsed={false}
           />
         </div>
 
